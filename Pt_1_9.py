466273f9b37a0a1e71fb71700d9f47d3c41d1fae
ArtemiySviridov <artemiy.sviridov.2004@gmail.com>
2023-07-02 23:31:49 +0700

    Pt_1_9: Concatenation and product
    This program counts a product, uses f-string to contact variables and string.

---
+++ Pt_1_9.py
@@ -0,0 +1,4 @@
+first_num = int(input("Введите число: "))
+second_num = int(input("Введите число: "))
+product_of_numbers = first_num * second_num
+print(f"Произведение чисел {first_num} и {second_num} равно {product_of_numbers}.")
