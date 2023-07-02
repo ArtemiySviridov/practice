650de9ec3ab5b4a83a228e92229df274f62247bb
ArtemiySviridov <artemiy.sviridov.2004@gmail.com>
2023-07-02 23:02:18 +0700

    Pt_1_2: Name, surname and length of string
    This program prints string with name and surname of user and length of this string.

---
+++ Pt_1_2.py
@@ -0,0 +1,4 @@
+enter_your_name = input("Введите Ваше имя: ")
+enter_your_surname = input("Введите Вашу фамилию: ")
+result = ' '.join([enter_your_name, enter_your_surname])
+print("Имя, фамилия: ", result, "\nДлина строки: ", len(result))
