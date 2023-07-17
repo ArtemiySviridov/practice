3f0cf6ce4d092b52edf7a48dca4b49d20ff5bd2f
ArtemiySviridov <artemiy.sviridov.2004@gmail.com>
2023-07-18 01:00:10 +0700

    Pt_4_2: Palindromes in string
    Processing of error E226

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -1,7 +1,7 @@
 def find_palindromes(message):
     all_palindromes = []
     for i in range(len(message)):
-        for j in range(i+1, len(message) + 1):
+        for j in range(i + 1, len(message) + 1):
             substring = message[i:j]
             if substring == substring[::-1] and len(substring) > 1:
                 all_palindromes.append(substring)
