b61df417e860afbe871d4e1534986bf51287ae6a
ArtemiySviridov <artemiy.sviridov.2004@gmail.com>
2023-07-18 00:31:09 +0700

    Pt_6_2: Parser of TOP-1000 top sellers games in Steam
    Changed parse_tags method.

--- Pt_6_2/Pt_6_2/spiders/Pt_6_2.py
+++ Pt_6_2/Pt_6_2/spiders/Pt_6_2.py
@@ -9,7 +9,7 @@ class SteamSpider(scrapy.Spider):
     parsed_games = 0
 
     def parse(self, response, **kwargs):
-        games = response.css('#search_resultsRows a::attr(href)')
+        games = response.css('a.search_result_row.ds_collapse_flag')
         for game in games:
             yield response.follow(game, callback=self.parse_tags, cookies={'birthtime': '943981201'})
 
@@ -18,13 +18,12 @@ class SteamSpider(scrapy.Spider):
             yield response.follow(self.url % self.page, callback=self.parse)
 
     def parse_tags(self, response):
-        if self.parsed_games == 1000:
+        if self.parsed_games < 1000:
+            yield {
+                'name': response.css("div.apphub_AppName::text").get(),
+                'url': response.url,
+                'tags': [tag.css("a::text").get().strip() for tag in response.css("a.app_tag")]
+            }
+        else:
             return
-
         self.parsed_games += 1
-
-        yield {
-            'appName': response.css('#appHubAppName::text').get(),
-            'link': response.url.split('/?')[0],
-            'tags': list(map(str.strip, response.css('a.app_tag::text').getall()))
-        }
